aa348e17919f3487ea0643d1c839887c53d4c9cf
kennyjung0223 <kenrohs@gmail.com>
2020-11-22 19:30:35 -0800

    split Sprite.h to Sprite.h and Sprite.cpp

--- Sprite.h
+++ Sprite.h
@@ -20,84 +20,15 @@ class Sprite: public TexRect{
 	bool done;
 
 public:
-	Sprite(const char* filename, int rows, int cols, float x, float y, float w, float h): TexRect(filename, x, y, w, h){
-		this->rows = rows;
-		this->cols = cols;
+	Sprite(const char* filename, int rows, int cols, float x, float y, float w, float h);
 
-		xinc = 1.0 / cols;
-		yinc = 1.0 / rows;
-
-		curr_row = 1;
-		curr_col = 1;
-
-		left = xinc * (curr_col - 1);
-		right = xinc * curr_col;
-		top = 1 - yinc * (curr_row - 1);
-		bottom = 1 - yinc * curr_row;
-
-		done = false;
-
-	}
-
-	void draw(float z = 0){
-		glBindTexture( GL_TEXTURE_2D, texture_id );
-		glEnable(GL_TEXTURE_2D);
-		
-		glBegin(GL_QUADS);
-		glColor4f(1, 1, 1, 1);
-		glTexCoord2f(left, bottom);
-		glVertex3f(x, y - h, z);
-		
-		glTexCoord2f(left, top);
-		glVertex3f(x, y, z);
-		
-		glTexCoord2f(right, top);
-		glVertex3f(x+w, y, z);
-		
-		glTexCoord2f(right, bottom);
-		glVertex3f(x+w, y - h, z);
-		
-		glEnd();
-		
-		glDisable(GL_TEXTURE_2D);
-	}
+	void draw(float z = 0);
 	
-	bool isDone () const {
-		return done;
-	}
+	bool isDone () const;
 
-	void reset(){
-		curr_row = 1;
-		curr_col = 1;
-		done = false;
-	}
+	void reset();
 
-	void advance(){
-		if (!done){
-			if (curr_col < cols){
-				curr_col++;
-			}
-			else{
-				if (curr_row < rows){
-					curr_col = 1;
-					curr_row++;
-				}
-				else{
-					done = true;
-					curr_row = 1;
-					curr_col = 1;
-				}
-				
-			}
-			left = xinc * (curr_col - 1);
-			right = xinc * curr_col;
-			top = 1 - yinc * (curr_row - 1);
-			bottom = 1 - yinc * curr_row;
+	void advance();
 
-		}
-	}
-
-	~Sprite(){
-
-	}
+	~Sprite();
 };
